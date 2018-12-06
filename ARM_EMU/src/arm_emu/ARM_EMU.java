07c9880b67243ee5808f44a7e147971641785382
Sanches1234 <36462138+Sanches1234@users.noreply.github.com>
2018-12-06 17:21:42 -0500

    Update ARM_EMU.java

--- ARM_EMU/src/arm_emu/ARM_EMU.java
+++ ARM_EMU/src/arm_emu/ARM_EMU.java
@@ -1,8 +1,55 @@
 package arm_emu;
 
 public class ARM_EMU {
-
+    
+   
     public static void main(String[] args)
+        
+    { 
+    JFrame frame = new JFrame("ARM EMulator");
+    frame.setSize(900, 1050);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    JLabel R1 = new JLabel("R1" + "\n");
+    JLabel R2 = new JLabel("R2" + "\n");
+    JLabel R3 = new JLabel("R3" + "\n");
+    JLabel R4 = new JLabel("R4" + "\n");
+    JLabel R5 = new JLabel("R5" + "\n");
+    JLabel R6 = new JLabel("R6" + "\n");
+    JLabel R7 = new JLabel("R7" + "\n");
+    JLabel R8 = new JLabel("R8" + "\n");
+    JLabel R9 = new JLabel("R9" + "\n");
+    JLabel R10 = new JLabel("R10" + "\n");
+    JLabel R11 = new JLabel("R11" + "\n");
+    JLabel R12 = new JLabel("R12" + "\n");
+    JLabel R13 = new JLabel("R13" + "\n");
+    frame.setResizable(false);
+        frame.add(R1);
+    frame.setVisible(true);
+        frame.add(R2);
+    frame.setVisible(true);
+        frame.add(R3);
+    frame.setVisible(true);
+        frame.add(R4);
+    frame.setVisible(true);
+      frame.add(R5);
+    frame.setVisible(true);
+      frame.add(R6);
+    frame.setVisible(true);
+      frame.add(R7);
+    frame.setVisible(true);
+      frame.add(R8);
+    frame.setVisible(true);
+      frame.add(R9);
+    frame.setVisible(true);
+      frame.add(R10);
+    frame.setVisible(true);
+      frame.add(R11);
+    frame.setVisible(true);
+      frame.add(R12);
+    frame.setVisible(true);
+      frame.add(R13);
+    frame.setVisible(true);
+    }
     /*{
         ARM leg= new ARM(10,16,12,4);
         int sp=0;
